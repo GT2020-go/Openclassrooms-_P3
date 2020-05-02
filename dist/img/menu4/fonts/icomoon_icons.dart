166c64da2b67e31aaee5f2d177c28ed34c15fe66
Gaetan Thereau <gaetan.thereau@gmail.com>
2020-05-02 08:43:03 +0200

    Update files structure

---
+++ dist/img/menu4/fonts/icomoon_icons.dart
@@ -0,0 +1,17 @@
+// Place fonts/icomoon.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: icomoon
+//      fonts:
+//       - asset: fonts/icomoon.ttf
+import 'package:flutter/widgets.dart';
+
+class Icomoon {
+  Icomoon._();
+
+  static const String _fontFamily = 'icomoon';
+
+  static const IconData menu4left = IconData(0x2906, fontFamily: _fontFamily);
+  static const IconData menu4right = IconData(0x2907, fontFamily: _fontFamily);
+}
